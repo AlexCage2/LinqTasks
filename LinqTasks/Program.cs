e902fbfb6b1e8fb45d12d3a6f9cbb27626f23613
alexcage2 <alexander.kirichenko.o@mail.ru>
2022-12-20 17:53:27 +0300

    Correct Connection String

--- LinqTasks/Program.cs
+++ LinqTasks/Program.cs
@@ -5,11 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 {
     /* Servives */
     /* Database Context Dependecy Injection */
-    //string dbHost = Environment.GetEnvironmentVariable("DB_HOST");
-    //string dbName = Environment.GetEnvironmentVariable("DB_NAME");
-    //string dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
-    //string connection = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword};Encrypt = false;";
-    string connection = builder.Configuration.GetConnectionString("DefaultConnection")!;
+    string dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+    string dbName = Environment.GetEnvironmentVariable("DB_NAME");
+    string dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+    string connection = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword};Encrypt = false;";
+    //string connection = builder.Configuration.GetConnectionString("DefaultConnection")!;
     builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(connection));
 
     /* MVC Dependecy Injection */
@@ -24,7 +24,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
 
     var context = services.GetRequiredService<DataContext>();
-    //context.Database.EnsureCreated();
+    context.Database.EnsureCreated();
     DbInitializer.Initialize(context);
 }
 
